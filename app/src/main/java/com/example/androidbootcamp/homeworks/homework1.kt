bab7647669080b3b4ba968b07935a838106a4cc1
Hasan Hüseyin Yılmaz <Hhyilmaz00@gmail.com>
2025-04-26 10:02:53 +0300

    Create homework1.kt
    homework1

---
+++ app/src/main/java/com/example/androidbootcamp/homeworks/homework1.kt
@@ -0,0 +1,51 @@
+package com.example.androidbootcamp.homeworks
+
+fun main(){
+    var city = "Istanbul"
+    val country = "Turkey"
+    var phone = "+905555555555"
+    val postalCode = "34000"
+    var email = "hhyilmazzt@gmail.com"
+    var profession = "Engineer"
+    var stockQuantity = 87
+    val customerName = "Muhammed Ali"
+    var balance = 371.45
+    val birthDate = "1996-07-21"
+    var salary = 78250
+    var maritalStatus = "Single"
+    var productReview = "Amazing product, highly recommend!"
+    val paymentDate = "2025-04-26"
+    var payment = 421.89
+    var orderQuantity = 2
+    val carModel = "BMW X1"
+    val bookTitle = "The Lord of the Rings: The Fellowship of the Ring"
+    val publishDate = "1954-07-29"
+    var discountAmount = 15.5
+    var roomCount = 1
+    val latitude = 39.0725
+    val longitude = 26.8906
+    val productName = "Wireless Headphones"
+    var mealPrice = 89.99
+    val brand = "Apple"
+    val musicTitle = "Bohemian Rhapsody"
+    val videoDuration = 418
+    var productRating = 4.8
+    val imageName = "profile_pic.jpg"
+    val fileFormat = "jpg"
+    var color = "Midnight Blue"
+    val colorCode = "#191970"
+    val phoneModel = "iPhone 15 Pro"
+    val screenSize = 6.1
+    val weight = 0.187
+    val nationalHoliday = "23 Nisan Ulusal Egemenlik ve Çocuk Bayramı"
+    val holiday = "Ramazan Bayramı"
+    val reservationDate = "2025-06-15"
+    val streetName = "Atatürk Caddesi"
+    val busLine = "Mavişehir - Konak 121"
+    var remainingMinutes = 7
+    val trackingCode = "TRK2025ABC"
+    var couponDuration = 45
+    val couponCode = "BAYRAM45"
+    val billingAddress = "Atatürk Mahallesi, 35600, İzmir, Türkiye"
+
+}
